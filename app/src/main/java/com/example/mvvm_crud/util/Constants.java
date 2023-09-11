1d61867479f60db8f6d76a25147c862f234c8ddf
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-09-12 06:53:32 +0700

    Build(Dependencies): added dependencies

--- app/src/main/java/com/example/mvvm_crud/util/Constants.java
+++ app/src/main/java/com/example/mvvm_crud/util/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final String Loading = "Mohon tunggu sebentar yaaa";
     public static final String ERROR_RESPONSE = "error";
     public static final String SOMETHING_WENT_WRONG = "Something went wrong";
+    public static final String API_KEY = "phplaravelcoii";
 }
